bcb3b35906f8c9d300f729ddb62616fb243f863b
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-28 14:26:38 +0600

    Create UserInterface.java

---
+++ UserInterface.java
@@ -0,0 +1,5 @@
+public interface UserInterface {
+    User[] getAllUser() throws Exception;
+    Announcements getAnnouncementsByUserId(int id);
+    Announcements getAnnouncementsByAddress(Address address);\
+}
