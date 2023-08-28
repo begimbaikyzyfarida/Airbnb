73d7b371a2179ff109ede6dcc625124190dde44c
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-28 14:26:03 +0600

    Create BookingInterface.java

---
+++ BookingInterface.java
@@ -0,0 +1,5 @@
+public interface BookingInterface {
+    void bookAnnouncement (boolean isBooked, Announcements announcements);
+    void unBookAnnouncement(boolean isBooked, Announcements announcements);
+
+}
