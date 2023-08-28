56fc64f5cc433e73c13561916e19bd652d3fc048
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-28 14:25:26 +0600

    Create AnnouncementInterface.java

---
+++ AnnouncementInterface.java
@@ -0,0 +1,10 @@
+public interface AnnouncementInterface {
+    String deleteAnnouncement(int id);
+    void updateAnnouncementById(int id, String newText);
+    Announcements getAnnouncementById(int id);
+    Announcements[] getAllAnnouncements();
+    Announcements[] sortAnnouncementsByRating();
+    Announcements[] sortByPrice();
+    void filterByType(String type);
+    void isBooked();
+}
